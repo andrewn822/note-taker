b1f897a85061f9231697125d62de4b2bf4a16ad4
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro-2.local>
2022-12-06 13:21:11 -0800

    server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001
+const path = require('path');
+
+// middleware
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/assets/index.html'))
+);
