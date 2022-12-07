90793c07d3d15e78c86f5adb89681f38fb326ac8
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro-2.local>
2022-12-06 16:50:50 -0800

    fsutils changes

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -9,7 +9,7 @@ const readFromFile = util.promisify(fs.readFile);
     @returns {void} 
 */  
 
-const writeToFile = (destination, context) => 
+const writeToFile = (destination, content) => 
     fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
         err ? console.error(err) : console.info(`\nData written to ${destination}`)
     );
