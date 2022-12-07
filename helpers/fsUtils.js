bdca97b7cb6ee6c0e67a8e7cb072e05a13c569bd
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro-2.local>
2022-12-06 17:02:28 -0800

    trying a delete change

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -33,4 +33,21 @@ const writeToFile = (destination, content) =>
     });
   };
   
+  app.delete('/api/notes/:id', (req, res) => {
+    console.info(req.method)
+    const delNoteID = req.params.id
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        //the result will be returned as an array with all of the notes except the note with id that was passed in as a param
+        const result = json.filter((note) => note.id !== delNoteID)
+        console.log(result)
+
+        //re-write the result array to db.json
+        writeToFile('./db/db.json', result)
+
+        //response to let client know delete request has been completed
+        res.json(`note with ${delNoteID} has been deleted`)
+    })
+})
   module.exports = { readFromFile, writeToFile, readAndAppend };
