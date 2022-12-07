23b6b1d98f47d7e3110ff69c7ec0bff286bc4d6c
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro-2.local>
2022-12-06 16:36:58 -0800

    changes

--- server.js
+++ server.js
@@ -5,15 +5,12 @@ const path = require('path');
 const { readFromFile,
     readAndAppend,
     writeToFile, } = require ('./helpers/fsUtils')
+
 // middleware
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-// get route for all *
-app.get('*', (req, rest) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -31,7 +28,7 @@ app.get('/api/notes', (req ,res) => {
 
 app.post('/api/notes', (req,res) => {
     const { title, text, } = req.body
-
+    
     if(title && text) {
         const newNote = {
             title,
@@ -43,8 +40,12 @@ app.post('/api/notes', (req,res) => {
     } else {
         console.log('no new notes posted')
     }
-
+    
 })
+// get route for all *
+app.get('*', (req, rest) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
 
 //listening on port
 app.listen(PORT, () =>
